3725ea3a1c39ddec93c0d7e68227ccec6752233b
Takayuki Konishi <seannos.takayuki@gmail.com>
2013-03-31 15:06:13 +0900

    remove dependency of AccessLogValve

--- src/main/java/net/seannos/valve/access/lamp/AccessLampValve.java
+++ src/main/java/net/seannos/valve/access/lamp/AccessLampValve.java
@@ -1,13 +1,14 @@
 package net.seannos.valve.access.lamp;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
-import javax.servlet.ServletException;
-import org.apache.catalina.LifecycleException;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
+
+import org.apache.catalina.valves.ValveBase;
+
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleListener;
+import org.apache.catalina.LifecycleException;
 import org.apache.catalina.util.LifecycleSupport;
-import org.apache.catalina.valves.AccessLogValve;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
@@ -16,46 +17,70 @@ import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
-public class AccessLampValve extends AccessLogValve {
+import javax.servlet.ServletException;
 
-	private String pinNumber;
-	@Override
-	public void start() throws LifecycleException {
-		gpio = GpioFactory.getInstance();
-		pin = gpio.provisionDigitalOutputPin(getPin(pinNumber), "MyLED",
-				PinState.LOW);
-		super.start();
-	}
+import java.lang.reflect.Field;
+import java.io.IOException;
+
+public class AccessLampValve
+    extends ValveBase
+    implements Lifecycle {
+
+    protected boolean started = false;
+    protected LifecycleSupport lifecycle = new LifecycleSupport(this);
 
 	GpioController gpio;
 	GpioPinDigitalOutput pin;
 
-    protected LifecycleSupport lifecycle = new LifecycleSupport(this);
-    
-	public AccessLampValve() {
+	private String pinNumber;
+
+
+	public void start() throws LifecycleException {
+
+        if (started)
+            throw new LifecycleException("accessLampValve.alreadyStarted");
+
+		gpio = GpioFactory.getInstance();
+		pin = gpio.provisionDigitalOutputPin(getPin(pinNumber), "MyLED",
+				PinState.LOW);
+
+        lifecycle.fireLifecycleEvent(START_EVENT, null);
+        started = true;
 	}
 
-	public void setPinNumber(String no) {
-		this.pinNumber = no;
-	}
-
-	public String getPinNumber() {
-		return pinNumber;
-	}
-
-	@Override
 	public void stop() throws LifecycleException {
+
+        if (!started)
+            throw new LifecycleException("accessLampValve.notStarted");
+
 		gpio.shutdown();
-		super.stop();
+
+        lifecycle.fireLifecycleEvent(STOP_EVENT, null);
+        started = false;
 	}
 
-	@Override
-	public void invoke(Request arg0, Response arg1) throws IOException,
+	public void invoke(Request request, Response response) throws IOException,
 			ServletException {
-		pin.high();
-		super.invoke(arg0, arg1);
-		pin.low();
+        if (started) {                
+		    pin.high();
+            getNext().invoke(request, response);
+		    pin.low();
+        } else {
+            getNext().invoke(request, response);
+        }
 	}
+
+    public void removeLifecycleListener(LifecycleListener listener) {
+        lifecycle.removeLifecycleListener(listener);
+    }
+
+    public LifecycleListener[] findLifecycleListeners() {
+        return lifecycle.findLifecycleListeners();
+    }
+
+    public void addLifecycleListener(LifecycleListener listener) {
+        lifecycle.addLifecycleListener(listener);
+    }
 
 	private static Pin getPin(String pinNumber) {
 		String raspiPinClass = RaspiPin.class.getName();
@@ -71,5 +96,14 @@ public class AccessLampValve extends AccessLogValve {
 			e.printStackTrace();
 			throw new RuntimeException();
 		}
+	}
+
+    // getter and setter
+	public void setPinNumber(String no) {
+		this.pinNumber = no;
+	}
+
+	public String getPinNumber() {
+		return pinNumber;
 	}
 }
